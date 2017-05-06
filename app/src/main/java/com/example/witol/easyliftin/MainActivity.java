c06a8b01ba8496dcfa55e70661e27afd6d97300a
Witold Leicht <Witold Leicht>
2017-05-06 21:23:43 +0200

    first commit

---
+++ app/src/main/java/com/example/witol/easyliftin/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.witol.easyliftin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newTraining(View view) {
+        Intent intent = new Intent(getApplicationContext(), NewTraining.class);
+        startActivity(intent);
+    }
+
+}
