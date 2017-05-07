241c92629fc0aa6802eea801b0069fa14fc94459
Witold Leicht <witold.leicht@gmail.com>
2017-05-07 02:21:27 +0200

    Created the view for adding new exercise. Added the navigation drawer to the main activity and implemented its adapter.

--- app/src/main/java/com/example/witol/easyliftin/MainActivity.java
+++ app/src/main/java/com/example/witol/easyliftin/MainActivity.java
@@ -7,19 +7,53 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] menu = {"New exercise", "New training"};
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menu));
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = null;
+                switch(i) {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), NewExercise.class);
+                        break;
+
+                    case 1:
+                        intent = new Intent(getApplicationContext(), NewTraining.class);
+                        break;
+                }
+                startActivity(intent);
+
+                //Toast.makeText(getApplicationContext(), "BUDYN TY KUHWOOOOO", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void newTraining(View view) {
+
+
+
+   /* public void newTraining(View view) {
         Intent intent = new Intent(getApplicationContext(), NewTraining.class);
         startActivity(intent);
-    }
+    }*/
 
 }
